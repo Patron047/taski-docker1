cbd4d54b826a421c6bfbec385a1426c8874b5a50
Андрей Федотов <schon2001@mail.ru>
2026-05-25 16:37:00 +0300

    Fix Nginx static paths for Taski admin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # Указываем корневую директорию для сборки статических файлов;
 # в контейнере это будет /app/collected_static
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = '/backend_static/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
